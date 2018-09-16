fix svd precision

discriminator.py
@@ -1,7 +1,11 @@
+import numpy as np
+import scipy.linalg
 import torch
 import torch.nn as nn
 
 __all__ = ["Discriminator"]
+
+GPU = torch.device("cuda:0")
 
 
 def _clip_elementwise(module):
@@ -10,10 +14,9 @@ def _clip_elementwise(module):
 
 
 def _clip_spectral(module):
-    with torch.no_grad():
-        u, s, vt = torch.svd(module.weight.data)
-        s.clamp_(0.0, 1.0)
-        module.weight.data.copy_(torch.einsum("ik,k,kj->ij", (u, s, vt)))
+    u, s, vt = scipy.linalg.svd(np.array(module.weight.data, dtype=np.float64))
+    w = np.einsum("ik,k,kj->ij", u, s.clip(0.0, 1.0), vt)
+    module.weight.data.copy_(torch.from_numpy(w).to(torch.float).to(GPU))
 
 
 class Discriminator(nn.Module):

train_muse_fast_text.py
@@ -2,9 +2,11 @@ import argparse
 import os
 from datetime import datetime
 
+import numpy as np
+import scipy.linalg
 import torch
-import torch.nn as nn
 import torch.autograd as autograd
+import torch.nn as nn
 import visdom
 from torch.utils.data import DataLoader
 from tqdm import trange
@@ -56,7 +58,7 @@ def _csls_nn(x, z, *, bs, k=10):
 
 
 def _orthogonal_project(m):
-    u, _, vt = torch.svd(m)
+    u, _, vt = scipy.linalg.svd(m)
     return u @ vt
 
 
@@ -70,28 +72,31 @@ def _refine(x, z, top, bs, mode="S2T"):
             p = _csls_nn(x, z, bs=bs)  # Long[n]
             p = torch.stack((torch.arange(x.shape[0], device=GPU), p), dim=1)  # Long[n, 2]
             p = p.masked_select((p.max(dim=1, keepdim=True)[0] < top).expand_as(p)).view(-1, 2)  # Long[?, 2]
-            print(p)
-            m = torch.einsum("ki,kj->ij", (x[p[:, 0]], z[p[:, 1]]))
+            xx, zz = np.array(x[p[:, 0]], dtype=np.float64), np.array(z[p[:, 1]], dtype=np.float64)
+            m = np.einsum("ki,kj->ij", xx, zz)
             w = _orthogonal_project(m)
         elif mode == "T2S":
             p = _csls_nn(z, x, bs=bs)  # Long[n]
             p = torch.stack((torch.arange(x.shape[0], device=GPU), p), dim=1)  # Long[n, 2]
             p = p.masked_select((p.max(dim=1, keepdim=True)[0] < top).expand_as(p)).view(-1, 2)  # Long[?, 2]
-            m = torch.einsum("ki,kj->ij", (x[p[:, 1]], z[p[:, 0]]))
+            xx, zz = np.array(x[p[:, 1]], dtype=np.float64), np.array(z[p[:, 0]], dtype=np.float64)
+            m = np.einsum("ki,kj->ij", xx, zz)
             w = _orthogonal_project(m)
         elif mode == "both":
             p = _csls_nn(x, z, bs=bs)  # Long[n]
             p = torch.stack((torch.arange(x.shape[0], device=GPU), p), dim=1)  # Long[n, 2]
             p = p.masked_select((p.max(dim=1, keepdim=True)[0] < top).expand_as(p)).view(-1, 2)  # Long[?, 2]
-            m1 = torch.einsum("ki,kj->ij", (x[p[:, 0]], z[p[:, 1]]))
+            xx, zz = np.array(x[p[:, 0]], dtype=np.float64), np.array(z[p[:, 1]], dtype=np.float64)
+            m1 = np.einsum("ki,kj->ij", xx, zz)
             p = _csls_nn(z, x, bs=bs)  # Long[n]
             p = torch.stack((torch.arange(x.shape[0], device=GPU), p), dim=1)  # Long[n, 2]
             p = p.masked_select((p.max(dim=1, keepdim=True)[0] < top).expand_as(p)).view(-1, 2)  # Long[?, 2]
-            m2 = torch.einsum("ki,kj->ij", (x[p[:, 1]], z[p[:, 0]]))
+            xx, zz = np.array(x[p[:, 1]], dtype=np.float64), np.array(z[p[:, 0]], dtype=np.float64)
+            m2 = np.einsum("ki,kj->ij", xx, zz)
             w = _orthogonal_project((m1 + m2) * 0.5)
         else:
             raise Exception(f"procrustes mode {mode} does not exist")
-        return x @ w, z
+        return x @ torch.from_numpy(w).to(torch.float).to(GPU), z
 
 
 class Trainer:
@@ -438,7 +443,7 @@ def main():
             torch.save({"dico": dic1, "vectors": emb1}, os.path.join(out_path, f"{params.tgt_lang}-epoch{epoch}.pth"))
     x = normalize_embeddings(best_x, params.normalize_mid)
     z = normalize_embeddings(best_z, params.normalize_mid)
-    torch.save([x, z], os.path.join("tmp.pth"))
+    # torch.save([x, z], os.path.join("tmp.pth"))
     for i in trange(params.r_n_steps):
         x, z = _refine(x, z, top=params.r_top, bs=params.r_bs, mode=params.r_mode)
         valid_metric = dist_mean_cosine(x, z)